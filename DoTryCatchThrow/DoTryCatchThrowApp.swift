476606dddc65f0e4d1823db1af895a743eb5d402
DucTuan <tuanhypc02@gmail.com>
2024-06-21 17:00:37 +0700

    do try catch and throw

---
+++ DoTryCatchThrow/DoTryCatchThrowApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoTryCatchThrowApp.swift
+//  DoTryCatchThrow
+//
+//  Created by macOS on 21/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct DoTryCatchThrowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
